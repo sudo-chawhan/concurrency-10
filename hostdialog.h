bdb1b198573d4c5c0dfd3ec296644ae012af1cfc
sid20071997 <cs16btech11035@iith.ac.in>
2018-03-06 17:18:14 +0530

    feature added

--- hostdialog.h
+++ hostdialog.h
@@ -2,6 +2,10 @@
 #define HOSTDIALOG_H
 
 #include <QDialog>
+#include <QString>
+#include "serverscreen.h"
+
+extern serverScreen *ss;
 
 namespace Ui {
 class hostdialog;
@@ -12,8 +16,11 @@ class hostdialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit hostdialog(QWidget *parent = 0);
+     hostdialog(QString s,QWidget *parent = 0);
     ~hostdialog();
+
+private slots:
+     void on_pushButton_clicked();
 
 private:
     Ui::hostdialog *ui;
