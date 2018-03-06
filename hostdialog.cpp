d23024e32e4b485598816db3a7077683e4acc963
sid20071997 <cs16btech11035@iith.ac.in>
2018-03-06 17:19:25 +0530

    feature added

--- hostdialog.cpp
+++ hostdialog.cpp
@@ -1,14 +1,23 @@
 #include "hostdialog.h"
 #include "ui_hostdialog.h"
+#include <string>
 
-hostdialog::hostdialog(QWidget *parent) :
+hostdialog::hostdialog(QString s,QWidget *parent) :
     QDialog(parent),
     ui(new Ui::hostdialog)
 {
     ui->setupUi(this);
+    this->setWindowTitle("HOST WINDOW");
+    ui->label->setText("ENTER THE IP IN JOIN MENU :"+s);
 }
 
 hostdialog::~hostdialog()
 {
     delete ui;
+}
+
+void hostdialog::on_pushButton_clicked()
+{
+    this->close();
+    ss =new serverScreen();
 }
