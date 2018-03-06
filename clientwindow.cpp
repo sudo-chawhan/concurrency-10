db574c0c30fe1e26ded6236ebf09b54ab890a089
sid20071997 <cs16btech11035@iith.ac.in>
2018-03-06 21:22:25 +0530

    client window added

--- clientwindow.cpp
+++ clientwindow.cpp
@@ -6,9 +6,25 @@ clientwindow::clientwindow(QWidget *parent) :
     ui(new Ui::clientwindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("CLIENT WINDOW");
+    ui->label->setText("ENTER THE IP ADDRESS OF SERVER");
 }
 
 clientwindow::~clientwindow()
 {
     delete ui;
+}
+
+
+void clientwindow::on_pushButton_clicked()
+{
+    ip=ui->lineEdit->text();
+    if(ip=="hihi")
+    {
+        game =new Game();
+    }
+    else
+    {
+        qDebug()<<"WRONG IP ADDRESS ENTERED";
+    }
 }
